f2159f3fb65c6fe9cd7c66b49ac0d7dd4a7c2af5
Agg242 <olrick.lefebvre@olrick.fr>
2021-01-15 18:03:00 +0100

    Detection now based on AttnFlags

--- detect_vampire.s
+++ detect_vampire.s
@@ -3,32 +3,12 @@
     INCLUDE "exec/execbase.i"
 
 main:
-    moveq #5,d0                 ; WARN return code by default
-    move.l 4.w,a6               ; execbase
-    move.l MemList+LH_TAILPRED(a6),d1 ; end marker
-    move.l MemList+LH_HEAD(a6),a0     ; first node
-nodes_loop:
-    cmp.l LN_PRED(a0),d1        ; end of list ?
-    beq end
-    move.l LN_NAME(a0),a1       ; mem node name
-    move.l #vampire_name,a2     ; Vampire mem name
-    move.w #name_end-vampire_name-1,d2    ; name len
-next_char:
-    cmp.b (a1)+,(a2)+           ; compare strings
-    dbne d2,next_char
-
-    tst.w d2
-    bpl next_node               ; >=0 ? compare not completed ?
-    clr.l d0                    ; SUCCESS return code
-    bra end
-
-next_node:
-    move.l LN_SUCC(a0),a0
-    bra nodes_loop
-
+    moveq #5,d0             ; WARN return code by default
+    move.l 4.w,a6           ; execbase
+    move.w AttnFlags(a6),d1
+    btst #10,d1             ; 68080 bit
+    beq.s end
+    moveq #0,d0
 end:
     rts
 
-vampire_name:
-    dc.b "VampireFastMem",0
-name_end:
